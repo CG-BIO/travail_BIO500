dcaf0ead184fcac33ecdc074c15e273441a3c591
CG-BIO <camille.gagnon8@hotmail.com>
2023-04-17 21:29:31 -0400

    à l'aide

--- graphiques.R
+++ graphiques.R
@@ -1,19 +1,20 @@
 install.packages("igraph")
 library(igraph)
-##doit trouver une façon d'ajouter les données à L
-g <- graph.adjacency(L)
-plot(g)
+
+##doit trouver une façon d'ajouter les données à L!!!
+reseau <- graph.adjacency(L)
+plot(reseau)
 ##met des couleurs
 col.vec <- heat.colors(S)
 ##ajoute les couleurs au graphique
-V(g)$color = col.vec[rk]
+V(reseau)$color = col.vec[rk]
 
 #faire un ordre de grandeur
 col.vec <- seq(10, 25, length.out = S)
 # Attribuer ordre de grandeur au graphique
-V(g)$size = col.vec[rk]
+V(reseau)$size = col.vec[rk]
 
 #faire un layout en forme de cercle
-plot(g, vertex.label=NA, edge.arrow.mode = 0,
+plot(reseau, vertex.label=NA, edge.arrow.mode = 0,
      vertex.frame.color = NA,
-     layout = layout.circle(g))
+     layout = layout.circle(reseau))
