fff203d6a28184fd8677dd98f0c47ec2e06dad14
CG-BIO <camille.gagnon8@hotmail.com>
2023-04-19 22:33:48 -0400

    Roulement de tambours..........
    Les targets ne fonctionnent tourjours pas!!!!!

--- _targets.R
+++ _targets.R
@@ -2,6 +2,7 @@
 # Charger les packages
 library(targets)
 library(tarchetypes)
+library(visNetwork) #pour pouvoir utiliser la commande tar_glimpse (qui nous permet de voir notre réseau)
 
 # Créer une fonction pour le nettoyage et l'assemblage des données
 prep_donnees <- function(data_files) {
@@ -16,31 +17,35 @@ prep_donnees <- function(data_files) {
   return(data)
 }
 
+##Pourquoi on veut aller chercher notre fichier d'analyses???????
 # Importer le fichier source
-source("R/analyse.R")
+source("analyses.R") ##ne trouve pas le nombre de liens pcq manque des info dans analyses
+
+source("prep_donnees.R")
 
 # Créer les tragets du criss
 tar_option_set(packages = c("rmarkdown","knitr"))
+
 list(
   tar_target(
     name = path, # Cible
-    command = "./data_collab", # Dossier contenant les fichiers de données
+    command = "./data", # Dossier contenant les fichiers de données
     format = "file" # Format de la cible
   ),
+  
   tar_target(
     name = file_paths, # Cible
     command = list.files(path, full.names = TRUE) # Liste les fichiers dans le dossier
   ),
-  tar_target(
-    name = data, # Cible pour le modèle
-    command = prep_donnees(file_paths) # Jointure des jeux de données
-  ),
+  
+  tar_target(nettoyage,prep_donnees(file_paths)),
+
   tar_target(
     name = resultat_modele, # Cible pour le modèle
     command = mon_modele(data) # Exécution de l'analyse
   ),
   tar_render(
     name = rapport, # Cible du rapport
-    path = "rapport/rapport.Rmd" # Le path du rapport à renderiser
+    path = "rapport.Rmd" # Le path du rapport à renderiser
   )
 )
