13560c864b44095f1b51e7e8a958d6a808482056
CG-BIO <camille.gagnon8@hotmail.com>
2023-04-20 12:47:45 -0400

    résoudre conflits sql

--- sql_analyses.R
+++ sql_analyses.R
@@ -1,4 +1,4 @@
-fct_sql <- function () {
+fct_analyse <- function () {
 
   ## CREER LA BASE DE DONNEES
   
@@ -101,5 +101,21 @@ head(nb_liens_paires)
 write.csv(nb_liens, file = "nb_liens.csv", row.names=FALSE)
 write.csv(nb_liens_paires, file = "nb_liens_paires.csv", row.names=FALSE)
 
-return(fct_sql)
+# Calculer le nombre d'étudiants
+nrow(nb_liens) # 163 étudiants
+
+# Calculer le nombre de liens
+sum(nb_liens_paires$nb_liens) # 3193
+
+# Connectance du réseau
+# Nombre de connexions existantes par rapport au nombre total de connexions possibles dans un réseau donné
+nrow(nb_liens) * nrow(nb_liens) # 26 569 connexions possibles
+connectance <- sum(nb_liens_paires$nb_liens) / (nrow(nb_liens) * nrow(nb_liens))
+connectance # 0.1201777
+
+# Nombre de liens moyens par étudiant
+moy_lien <- mean(nb_liens$nb_liens) # 19.6319
+var(nb_liens$nb_liens) # 580.3081
+
+return(fct_analyse)
 }
