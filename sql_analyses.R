390ac055984eb35b5e972b1938c2bfb0efc6bb4a
CG-BIO <camille.gagnon8@hotmail.com>
2023-04-20 12:11:25 -0400

    ajout sql aux analyses

--- sql_analyses.R
+++ sql_analyses.R
@@ -1,5 +1,105 @@
-fct_analyse -> function () {
+fct_analyse <- function () {
 
+  ## CREER LA BASE DE DONNEES
+  
+  # Aller chercher le package
+  library(RSQLite)
+  
+  # Etablir la connexion
+  con <- dbConnect(SQLite(), dbname="projet_session.db")
+  
+  # Creer la table etudiant
+  etudiant_sql <- "
+CREATE TABLE etudiant (
+  prenom_nom                  VARCHAR(50),
+  prenom                      VARCHAR(30),
+  nom                         VARCHAR(30),
+  region_administrative       VARCHAR(40),
+  regime_coop                 BOLEAN,
+  formation_prealable         VARCHAR(20),
+  annee_debut                 VARCHAR(5),
+  programme                   VARCHAR(6),
+  PRIMARY KEY (prenom_nom)
+);"
+  dbSendQuery(con,etudiant_sql)
+  
+  # Creer la table cours
+  cours_sql <- "
+CREATE TABLE cours (
+  sigle                       VARCHAR(6),
+  optionnel                   BOLEAN,
+  credits                     INTEGER,
+  PRIMARY KEY (sigle)
+);"
+  dbSendQuery(con,cours_sql)
+  
+  # Creer la table collab
+  collab_sql <- "
+CREATE TABLE collab (
+  etudiant1   VARCHAR(50),
+  etudiant2   VARCHAR(50),
+  sigle       VARCHAR(6),
+  session     VARCHAR(5),
+  PRIMARY KEY (etudiant1, etudiant2, sigle, session)
+  FOREIGN KEY (etudiant1) REFERENCES etudiant(prenom_nom),
+  FOREIGN KEY (etudiant2) REFERENCES etudiant(prenom_nom),
+  FOREIGN KEY (sigle)     REFERENCES cours(sigle)
+);"
+  dbSendQuery(con,collab_sql)
+  
+  # Joindre les tables ensemble dans un base de donnees nommee "projet_session.db"
+  dbListTables(con)
+  
+  
+  ## INJECTER LES DONNEES
+  
+  
+  # Enregistrer les trois grandes bases de donnees ("cours", "collab", "etudiant") en .csv
+  write.csv(cours, file = "cours.csv", row.names=FALSE)
+  write.csv(collab, file = "collab.csv", row.names=FALSE)
+  write.csv(etudiant, file = "etudiant.csv", row.names=FALSE)
+  
+  # Renommer les bases de donnees pour utiliser avec SQL (juste pour pas fuck up le chien)
+  bd_cours <- read.csv(file = "cours.csv")
+  bd_collab <- read.csv(file = "collab.csv")
+  bd_etudiant <- read.csv(file = "etudiant.csv")
+  
+  
+  # Injecter les donnees dans les bases de donnees SQL --> si problème, voir l'argument "append"
+  
+  dbWriteTable(con, name = "cours", value = bd_cours, overwrite = TRUE, row.names = FALSE)
+  dbWriteTable(con, name = "collab", value = bd_collab, overwrite = TRUE, row.names = FALSE)
+  dbWriteTable(con, name = "etudiant", value = bd_etudiant, overwrite = TRUE, row.names = FALSE)
+  
+  
+  ## FAIRE LES REQUETES DEMANDEES
+  
+  
+  # Nombre de liens par etudiant
+  sql_requete <- "
+  SELECT etudiant1, count(etudiant2) AS nb_liens
+  FROM collab
+  GROUP BY etudiant1
+  ORDER BY nb_liens DESC;"
+  nb_liens <- dbGetQuery(con, sql_requete)
+  head(nb_liens)
+  
+  # Nombre de liens par paire d'etudiants
+  sql_requete <- "
+SELECT c1.etudiant1, c1.etudiant2, COUNT(*) AS nb_liens
+FROM collab c1
+WHERE EXISTS (
+  SELECT *
+  FROM collab c2
+  WHERE c2.etudiant1 = c1.etudiant2 AND c2.etudiant2 = c1.etudiant1
+)
+GROUP BY c1.etudiant1, c1.etudiant2
+ORDER BY nb_liens DESC;"
+nb_liens_paires <- dbGetQuery(con, sql_requete)
+head(nb_liens_paires)
+
+write.csv(nb_liens, file = "nb_liens.csv", row.names=FALSE)
+write.csv(nb_liens_paires, file = "nb_liens_paires.csv", row.names=FALSE)
 
 # Calculer le nombre d'étudiants
 nrow(nb_liens) # 163 étudiants
