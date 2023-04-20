64bca12cac257e3a0d78701576f2449a9c9566a3
CG-BIO <camille.gagnon8@hotmail.com>
2023-04-20 13:29:35 -0400

    création de la fonction préliminaire pour les graphiques

--- graphiques.R
+++ graphiques.R
@@ -1,20 +1,6 @@
-install.packages("igraph")
-library(igraph)
+#codes pour les figures
 
-##doit trouver une façon d'ajouter les données à L!!!
-reseau <- graph.adjacency(L)
-plot(reseau)
-##met des couleurs
-col.vec <- heat.colors(S)
-##ajoute les couleurs au graphique
-V(reseau)$color = col.vec[rk]
-
-#faire un ordre de grandeur
-col.vec <- seq(10, 25, length.out = S)
-# Attribuer ordre de grandeur au graphique
-V(reseau)$size = col.vec[rk]
-
-#faire un layout en forme de cercle
-plot(reseau, vertex.label=NA, edge.arrow.mode = 0,
-     vertex.frame.color = NA,
-     layout = layout.circle(reseau))
+fct_figures <- function(){
+  
+  return(fct_figures)
+}
