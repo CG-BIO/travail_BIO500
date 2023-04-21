e8d2301e54244fcdd0f93e7a1e85ba726938745f
CG-BIO <128488241+CG-BIO@users.noreply.github.com>
2023-04-21 18:38:26 -0400

    Delete import_file.R

--- import_file.R
+++
@@ -1,9 +0,0 @@
-import_the_data = function(file_paths){
-  imported_data <- lapply(file_paths, function(x) {
-    if (grepl(".csv", x)) {
-      read.table(x, sep = ",")
-    } else {
-      read.table(x, sep = ";")
-    }
-  })
-}
